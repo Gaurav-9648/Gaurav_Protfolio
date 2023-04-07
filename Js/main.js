180d7b43e297c49a7faa2016e52d60684da09adf
Gaurav-9648 <gm2897937@gmail.com>
2023-04-07 13:37:25 +0530

    new changes

---
+++ Js/main.js
@@ -0,0 +1,21 @@
+
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+} 
+
+
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+ 
+
+
